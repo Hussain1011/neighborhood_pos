699840dc78e5734f19a0b63b538ff5410a0571ec
Hussain <rana.h10111992@gmail.com>
2025-01-05 18:18:36 +0500

    first commit

---
+++ neighborhood_pos/neighborhood_pos/doctype/automated_bom_manufacturing/test_automated_bom_manufacturing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ERPNext and contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAutomatedBOMManufacturing(FrappeTestCase):
+	pass
---
+++ neighborhood_pos/neighborhood_pos/doctype/branch_requisition_template/test_branch_requisition_template.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ERPNext and contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBranchRequisitionTemplate(FrappeTestCase):
+	pass
